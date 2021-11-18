e259a26e319576e59cd158149d0bd0d22c61289d
Pamela_Pereira <93348050+PamelaCP@users.noreply.github.com>
2021-11-18 15:28:42 -0300

    exercicio 6 e 8

--- src/challenges.js
+++ src/challenges.js
@@ -10,27 +10,39 @@ function calcArea(base, altura) {
 }
 // Desafio 3
 function splitSentence(param) {
-  return param.split(' ');// referência: https://www.devmedia.com.br/javascript-split-dividindo-separando-strings/39254//
+  return param.split(' '); // referência: https://www.devmedia.com.br/javascript-split-dividindo-separando-strings/39254//
 }
 
 // Desafio 4
-function concatName(recebe) {
-  let firstName = recebe[0];
-  let lastName = recebe[recebe.length - 1];
-  let strName = `${lastName},${firstName}`;
-  return concatName;
-}
+let name = ['Lucas', 'Cassiano', 'Ferraz', 'Paolillo'];
 
+function concatName(name) {
+  let novo = '';
+  novo = name[name.length - 1] + ', ' + name[0];
+  return novo;
+}
 // Desafio 5
 function footballPoints(wins, ties) {
   let footballPoints = wins * 3 + ties;
-  return footballPoints;
+  return footballPoints; // ou return wins * 3 + ties;
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(numbers) {
+  let maiorNumbers = numbers[0];
+  let numbRepit = 0;
+  for (let i = 1; i <= numbers.length; i += 1) {
+    if (maiorNumbers < numbers[i]) {
+      maiorNumbers = numbers[i];
+    }
+  }
+  for (let index = 0; index <= numbers.length; index += 1) {
+    if (maiorNumbers == numbers[index]) {
+      numbRepit += 1;
+    };
+  }
 
+  return numbRepit;
 }
 
 // Desafio 7
@@ -39,14 +51,26 @@ function catAndMouse() {
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(arrNumbers) {
+  let processed = [];
+  arrNumbers.forEach((element) => {
+    if (element % 3 === 0 && element % 5 === 0) {
+      processed.push('');
+    } else if (element % 5 === 0) {
+      processed.push('buzz');
+    } else if (element % 3 === 0) {
+      processed.push('fizz!');
+    } else {
+      processed.push('bug!');
+    }
+  });
+  return processed;
 }
-
 // Desafio 9
 function encode() {
   // seu código aqui
 }
+
 function decode() {
   // seu código aqui
 }
@@ -58,7 +82,6 @@ module.exports = {
   concatName,
   decode,
   encode,
-  fizzBuzz,
   footballPoints,
   highestCount,
   splitSentence,
